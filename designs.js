8766f811786a27222ec45796b3ca7cf828d80515
Antonio Riccelli <anton.riccelli@gmail.com>
2021-01-16 20:02:07 +0100

    amend functions

--- designs.js
+++ designs.js
@@ -1,43 +1,42 @@
-
-var colorInput = document.getElementById("colorPicker");
+var colorInput = document.getElementById("colorPicker").value;
 var selectTable = document.getElementById("pixelCanvas");
 var formInput = document.getElementById("sizePicker");
 var heightInput = document.getElementById("inputHeight");
 var widthInput = document.getElementById("inputWidth");
 
 
-function makeGrid(height, width) {
+formInput.addEventListener("submit", function (e) {
+    selectTable.innerHTML = "";
 
-    for (var i = 0; i < heightInput; i++) {
-        var row = selectTable.insertRow(i);
-          for (var n = 0; n < widthInput; n++) {
-            var cell = row.insertCell(n);
-           
-    }
-    
-}
-
-}
-
-makeGrid(heightInput, widthInput);
- cell.addEventListener("click", (e) => {
-    cell.style.backgroundColor = color.value;
-})
-
-
-
-
-formInput.addEventListener("click", function(e) {
-    
     e.preventDefault();
-    
+
     var heightInput = document.getElementById("inputHeight");
     var widthInput = document.getElementById("inputWeight");
-    selectTable.firstElementChild.remove();
-  
+
+
     makeGrid(heightInput, widthInput);
-  
+    addColor(colorInput);
 });
+
+
+function makeGrid(height, width) {
+
+    for (var i = 0; i < heightInput.value; i++) {
+        var row = selectTable.insertRow();
+        for (var n = 0; n < widthInput.value; n++) {
+            var cell = row.insertCell();
+        }
+    }
+}
+
+function addColor(color) {
+var allCells = document.querySelectorAll("td");
+for (var i = 0; i < allCells; i++) {
+    allCells[i].addEventListener("click", function(e) {
+        allCells[i].style.backgroundColor = colorInput.value;
+    })
+ }
+}
 
 
 
