09f184bd257c4ce32675613d89ab63c2e726a5bd
Antonio Riccelli <anton.riccelli@gmail.com>
2021-01-18 13:14:01 +0100

    add comments

--- designs.js
+++ designs.js
@@ -1,20 +1,28 @@
+// * Initial variables assignments, names are self-explanatory
 let colorInput = document.getElementById("colorPicker");
 let selectTable = document.getElementById("pixelCanvas");
 let formInput = document.getElementById("sizePicker");
 let heightInput = document.getElementById("inputHeight");
 let widthInput = document.getElementById("inputWidth");
 
-
+/*
+* @description collects form input information, passes it to functions to create table and assign color
+* @param {event} e 
+*/
 formInput.addEventListener("submit", function (e) {
     selectTable.innerHTML = "";
     e.preventDefault();
-    let heightInput = document.getElementById("inputHeight");
-    let widthInput = document.getElementById("inputWeight");
+    // let heightInput = document.getElementById("inputHeight");
+    // let widthInput = document.getElementById("inputWeight");
     makeGrid(heightInput, widthInput);
     addColor(colorInput);
 });
 
-
+/*
+* @description creates table
+* @param {number} height 
+* @param {number} width
+*/
 function makeGrid(height, width) {
     for (let i = 0; i < heightInput.value; i++) {
         let row = selectTable.insertRow();
@@ -24,6 +32,10 @@ function makeGrid(height, width) {
     }
 }
 
+/*
+* @description colours cells, adding event listeners for "mousedown", "mousemove" and "mouseup"
+* @param {value} color
+*/
 function addColor(color) {
     let allCells = document.querySelectorAll("td");
     let isDrawing = false;
